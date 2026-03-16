a75f14441e46c08911de107fb184da08fe3668d9
Pst Abbey <pstabbey71@gmail.com>
2026-03-16 21:43:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9569630022-dbad1",
+  "appId": "1:299316705986:web:18c598232112ae57ead95b",
+  "apiKey": "AIzaSyA6ZCJRe7Q_n5TAzhCWTxUYmb8sSMj9NOU",
+  "authDomain": "studio-9569630022-dbad1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "299316705986"
+};
